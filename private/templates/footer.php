27cacd10324b88c3e214cf88897cf00d681ccd6b
JoeThomas42 <132302844+JoeThomas42@users.noreply.github.com>
2025-03-09 20:03:52 -0400

    Implimented basic footer content

--- private/templates/footer.php
+++ private/templates/footer.php
@@ -1,5 +1,14 @@
 <?php
 
 echo "</div>"; // Close page-wrapper
+
+// Start footer section
+echo "<footer class='footer-container'>
+    <div class='footer-content'>
+        <h3>StageWrite</h3>
+        <p>The ultimate stage plot management tool for event production.</p>
+    </div>
+</footer>";
+
 echo "</body>";
 echo "</html>";
