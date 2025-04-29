567e22920d77bb57ab214824b0da5c2c8365423a
Joseph Thomas <132302844+JoeThomas42@users.noreply.github.com>
2025-04-29 12:31:28 -0400

    Update config.php

--- private/config/config.php
+++ private/config/config.php
@@ -7,8 +7,8 @@ $db_user = 'root';
 $db_password = '';
 
 // reCAPTCHA Config
-define('RECAPTCHA_SITE_KEY', '6Ld9xSQrAAAAADjwte96Or82zUJd-6Kfy-eNgJ73');
-define('RECAPTCHA_SECRET_KEY', '6Ld9xSQrAAAAAOcAFF-xkwG4pge-j609vVjh44Qt');
+define('RECAPTCHA_SITE_KEY', '');
+define('RECAPTCHA_SECRET_KEY', '');
 
 // Other configuration constants
 define('DB_HOST', $db_host);
